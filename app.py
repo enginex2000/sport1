657c023fa1fbc3d73149fd981ad1b80e6e6dd0c9
Phutthiphong Lakun <laphutthiphong@centralpattana.co.th>
2025-09-18 04:31:34 +0700

    Initial commit

--- app.py
+++ app.py
@@ -239,5 +239,4 @@ def toggle_maintenance():
 # ------------------------- Main -------------------------
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))  # ใช้ PORT จาก environment หรือ 5000
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
